92edd2672a6334bf0eb0227c723788633407425d
WesleyMiranda <wesleywmm@hotmail.com>
2023-04-09 21:30:49 -0300

    classesDeArquivo Criado

---
+++ ArquivoJava/Arquivo_Java.java
@@ -0,0 +1,122 @@
+package ArquivoJava;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Arquivo_Java {
+    
+    private String nomearquivo;
+    private RandomAccessFile arquivo;
+
+    public Arquivo_Java(String nomearquivo)
+    {
+        try
+        {
+            arquivo = new RandomAccessFile(nomearquivo, "rw");
+        } catch (IOException e)
+        { }
+    }
+
+    public void truncate(long pos) //desloca eof
+    {
+        try
+        {
+            arquivo.setLength(pos * Registro.length());
+        } catch (IOException exc)
+        { }
+    }
+
+    //semelhante ao feof() da linguagem C
+    //verifica se o ponteiro esta no <EOF> do arquivo
+    public boolean eof()  
+    {
+        boolean retorno = false;
+        try
+        {
+            if (arquivo.getFilePointer() == arquivo.length())
+                retorno = true;                               
+        } catch (IOException e)
+        { }
+        return (retorno);
+    }
+
+    //insere um Registro no final do arquivo, passado por parametro
+    public void inserirRegNoFinal(Registro reg)
+    {
+        seekArq(filesize());//ultimo byte
+        reg.gravaNoArq(arquivo);
+    }
+
+    public int filesize()
+    {
+        try 
+        {
+            return (int)arquivo.length()/Registro.length();
+        } 
+        catch (Exception e)
+        {
+            return 0;
+        }
+           
+    }
+
+    public void exibirArq()
+    {
+        int i;
+        Registro aux = new Registro();
+        seekArq(0);
+        i = 0;
+        while (!this.eof())
+        {
+            System.out.println("Posicao " + i);
+            aux.leDoArq(arquivo);
+            aux.exibirReg();
+            i++;
+        }
+    }
+
+    public void exibirUmRegistro(int pos)
+    {
+        Registro aux = new Registro();
+        seekArq(pos);
+        System.out.println("Posicao " + pos);
+        aux.leDoArq(arquivo);
+        aux.exibirReg();
+    }
+
+    public void seekArq(int pos)
+    {
+        try
+        {
+            arquivo.seek(pos * Registro.length());
+        } catch (IOException e)
+        { }
+    }
+
+    public void leArq()
+    {
+        int codigo, idade;
+        String nome;
+        codigo = Entrada.leInteger("Digite o c�digo");
+        while (codigo != 0)
+        {
+            nome = Entrada.leString("Digite o nome");
+            idade = Entrada.leInteger("Digite a idade");
+            inserirRegNoFinal(new Registro(codigo, nome, idade));
+            codigo = Entrada.leInteger("Digite o c�digo");
+        }
+    }
+
+    //.............................................................................
+    /*
+    insira aqui os metodos de Ordenacao;
+
+    */
+    public void executa()
+    {
+        leArq();
+        exibirArq();
+    }
+
+
+}
---
+++ ArquivoJava/Main.java
@@ -0,0 +1,12 @@
+package ArquivoJava;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Arquivo_Java a = new Arquivo_Java("c:\\arquivo.dat");
+        a.executa();
+
+    }
+
+}
